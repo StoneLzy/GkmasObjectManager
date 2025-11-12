501ffc8fb52f59d72480623982152c2a22872cf1
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-11-11 19:38:23 -0500

    [Fix] In USMVideo, probe input stream and reencode iff codec is 'mpeg1video'

--- GkmasObjectManager/media/video.py
+++ GkmasObjectManager/media/video.py
@@ -17,24 +17,49 @@ class GkmasUSMVideo(GkmasDummyMedia):
 
     def _convert(self, raw: bytes) -> bytes:
 
-        return subprocess.run(
+        probe = subprocess.run(
             [
-                "ffmpeg",
-                "-i",
-                "pipe:0",  # input bytestream
-                "-f",
-                self.converted_format,
-                "-c:v",
-                "copy",  # copy video stream without re-encoding
-                "-c:a",
-                "aac",  # was wavpack in source
-                "-b:a",
-                "1024k",  # highest possible
-                "-movflags",
-                "faststart+frag_keyframe",
-                # otherwise libx264 reports 'muxer does not support non seekable output'
-                "pipe:1",  # output bytestream
+                "ffprobe",
+                "pipe:0",
+                "-v",
+                "error",  # hide verbose info
+                "-select_streams",
+                "v:0",  # first video stream
+                "-show_entries",
+                "stream=codec_name",  # query
+                "-of",
+                "default=noprint_wrappers=1:nokey=1",  # hide formatting
             ],
+            input=raw,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+
+        cmd = [
+            "ffmpeg",
+            "-i",
+            "pipe:0",  # input bytestream
+            "-f",
+            self.converted_format,
+        ]
+        if probe.stdout.strip() == b"mpeg1video":  # should re-encode
+            cmd += ["-preset", "ultrafast"]  # -c:v inferred from -f
+        else:
+            cmd += ["-c:v", "copy"]
+        cmd += [
+            "-c:a",
+            "aac",  # was adpcm_adx in source
+            "-b:a",
+            "1024k",  # auto fallback to highest possible bitrate
+            "-movflags",
+            "faststart+frag_keyframe",
+            # otherwise libx264 reports 'muxer does not support non seekable output'
+            "pipe:1",  # output bytestream
+        ]
+
+        return subprocess.run(
+            cmd,
             input=raw,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,  # disposed
